21647c2b22eda0e2be558d5a0d12d50f3ef61834
kaiky-soares <162652707+kaiky-soares@users.noreply.github.com>
2025-03-19 17:31:54 -0300

    Add files via upload

---
+++ ex02.php
@@ -0,0 +1,10 @@
+<?php
+
+$valor = 300;
+
+$desconto = $valor * 0.27;
+
+echo "O valor do produto é: R$" $valor;
+echo "O valor com 27% de desconto é: R$" $desconto;
+
+?>
---
+++ ex03.php
@@ -0,0 +1,10 @@
+<?php
+
+$valor_produto = 400;
+$valor_total = $valor_produto * 0.16;
+$valor_parcelas = $valor_total / 10;
+
+echo "O valor total da compra com o acréscimo de 16% é: " $valor_total;
+echo "O valor de cada parcela será: R$ " $valor_parcelas;
+
+?>
